9b214b4374a092c731501f2907ccb60e697023ba
John-David Dalton <john.david.dalton@gmail.com>
2018-03-24 23:12:45 -0700

    Detect npm and yarn cli utils.

--- main.js
+++ main.js
@@ -1,52 +1,108 @@
-import Module from "module"
-
+import execa from "execa"
 import fleece from "golden-fleece"
-import fs from "fs-extra"
+import fs from "fs"
 import path from "path"
-import { shell } from "execa"
 
-const shellOptions = {
-  __proto__: null,
-  stdio: "inherit"
+const { env } = process
+const isWin = process.platform === "win32"
+
+const backSlashRegExp = /\\/g
+
+const npmBinRegExp = isWin
+  ? /[\\/]npm(\.cmd)?$/
+  : /\/npm$/
+
+const npmJsRegExp = isWin
+  ? /[\\/]node_modules[\\/]npm[\\/]bin[\\/]npm-cli\.js$/
+  : /\/node_modules\/npm\/bin\/npm-cli\.js$/
+
+const npxJsRegExp = isWin
+  ? /[\\/]node_modules[\\/]npm[\\/]bin[\\/]npm-cli\.js$/
+  : /\/node_modules\/npm\/bin\/npm-cli\.js$/
+
+const npxBinRegExp = isWin
+  ? /[\\/]npx(\.cmd)?$/
+  : /\/npx$/
+
+let bin = "yarn"
+
+function normalize(filename) {
+  return filename.replace(backSlashRegExp, "/")
 }
 
 Promise
   .resolve()
-  .then(() => shell("npm init", shellOptions))
-  .then(() => shell("npm i --save esm", shellOptions))
+  .then(() => {
+    if (npmJsRegExp.test(env.NPM_CLI_JS) ||
+        npxJsRegExp.test(env.NPX_CLI_JS) ||
+        npmBinRegExp.test(env._) ||
+        npxBinRegExp.test(env._)) {
+      bin = "npm"
+    }
+
+    return execa(bin, ["-v"], {
+      reject: false
+    })
+  })
+  .then((result) => {
+    if (result.failed) {
+      bin = bin === "npm" ? "yarn" : "npm"
+    }
+
+    return execa(bin, ["init"], {
+      stdio: "inherit"
+    })
+  })
+  .then(() => {
+    const args = bin === "npm"
+      ? ["i", "--save", "esm"]
+      : ["add", "esm"]
+
+    return execa(bin, args, {
+      stdio: "inherit"
+    })
+  })
   .then(() => {
     const pkgPath = path.resolve("package.json")
 
-    if (! fs.pathExistsSync(pkgPath)) {
+    if (! fs.existsSync(pkgPath)) {
       return
     }
 
     const pkgString = fs.readFileSync(pkgPath, "utf8")
     const pkgJSON = JSON.parse(pkgString)
+
     const main = pkgJSON.main || "index.js"
     const mainPath = require.resolve(main, { paths: ["."] })
+    const mainName = path.basename(mainPath)
+
+    const newMainName = mainName === "main.js"
+      ? "_" + mainName
+      : mainName
+
+    const newMainPath = path.resolve(path.dirname(mainPath), newMainName)
+
+    if (fs.existsSync(newMainPath)) {
+      return
+    }
 
     let relPath = path.relative(process.cwd(), mainPath)
 
     if (relPath.charCodeAt(0) !== 46 /* . */) {
-      relPath = "." + path.sep + relPath
+      relPath = "./" + relPath
     }
 
-    const newMainName = "_" + path.basename(mainPath)
-    const newMainPath = path.resolve(path.dirname(mainPath), newMainName)
-    const newMain = path.join(path.dirname(relPath), newMainName)
+    const newMain = path.resolve(path.dirname(relPath), newMainName)
 
     fs.writeFileSync(pkgPath, fleece.patch(pkgString, Object.assign({
-      main: newMain
+      main: normalize(newMain)
     }, pkgJSON)))
 
-    if (! fs.pathExistsSync(newMainPath)) {
-      fs.writeFileSync(newMainPath, [
-        '"use strict"',
-        "",
-        'require = require("esm")(module)',
-        'module.export = require("' + relPath + '")',
-        ''
-      ].join("\n"))
-    }
-})
+    fs.writeFileSync(newMainPath, [
+      '"use strict"',
+      "",
+      'require = require("esm")(module)',
+      'module.export = require("' + normalize(relPath) + '")',
+      ""
+    ].join("\n"))
+  })
