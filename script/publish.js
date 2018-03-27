5c9f5ca6d18f549d1018e759ec52445bc012f40e
John-David Dalton <john.david.dalton@gmail.com>
2018-03-26 21:55:23 -0700

    Add publish script.

---
+++ script/publish.js
@@ -0,0 +1,44 @@
+"use strict"
+
+const execa = require("execa")
+const fleece = require("golden-fleece")
+const fs = require("fs-extra")
+const path = require("path")
+
+const rootPath = path.resolve(__dirname, "..")
+const pkgPath = path.resolve(rootPath, "package.json")
+
+const defaultScripts = {
+  test: 'echo "Error: no test specified" && exit 1'
+}
+
+const fieldsToRemove = [
+  "devDependencies",
+  "private"
+]
+
+function cleanPackageJSON() {
+  return fs
+    .readFile(pkgPath, "utf8")
+    .then((content) => {
+      process.once("exit", () => fs.outputFileSync(pkgPath, content))
+
+      const pkgJSON = JSON.parse(content)
+      pkgJSON.scripts = defaultScripts
+      fieldsToRemove.forEach((field) => Reflect.deleteProperty(pkgJSON, field))
+      return fs.outputFile(pkgPath, fleece.patch(content, pkgJSON))
+    })
+}
+
+function publishPackage() {
+  return execa("npm", ["publish"], {
+    cwd: rootPath,
+    stdio: "inherit"
+  })
+}
+
+Promise
+  .resolve()
+  .then(cleanPackageJSON)
+  .then(publishPackage)
+  .catch(console.error)
